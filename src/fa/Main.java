72cb320b314fcf0be7b45243f4afe647c1624187
franz <franz@cea9dca5-4048-41bc-958f-9b301c446f7b>
2012-06-29 04:34:04 +0000

    Updated ProblemSets and Constants, testing overnight with this code to try and obtain decent results.
    git-svn-id: https://10.0.0.20/svn/projects/Firefly_Algorithm@1438 cea9dca5-4048-41bc-958f-9b301c446f7b

--- src/fa/Main.java
+++ src/fa/Main.java
@@ -21,7 +21,7 @@ public class Main implements FAConstants {
 		
 		ProblemSet fitnessFunction = new ProblemSet(NUM_DIMENSIONS);
 		
-		System.out.println("Initializing Fireflies");
+		//System.out.println("Initializing Fireflies");
 		fireflies = new Firefly[NUM_FIREFLIES];
 		for (int i = 0; i < NUM_FIREFLIES; i++) {
 			Firefly f = new Firefly(NUM_DIMENSIONS);
@@ -29,10 +29,27 @@ public class Main implements FAConstants {
 			fireflies[i] = f;
 		}
 		
-		long initTime = System.nanoTime();
-		System.out.println("\tFireflies initialized in " + (initTime - startTime) / 1000 + " us");
+		//long initTime = System.nanoTime();
+		//System.out.println("\tFireflies initialized in " + (initTime - startTime) / 1000 + " us");
 		
-		System.out.println("Running Algorithm");
+		
+		//System.out.println("Running Algorithm");
+		// Find max distance and reduce gamma accordingly
+		double maxDistance = 0, tempDist;
+		for (int i = 0; i < NUM_FIREFLIES; i++) {
+			for (int j = 0; j < i; j++) {
+				tempDist = 0;
+				for (int k = 0; k < NUM_DIMENSIONS; k++) {
+					tempDist += Math.pow(fireflies[j].getPosition()[k]-fireflies[i].getPosition()[k],2);
+				}
+				tempDist = Math.sqrt(tempDist);
+				if (tempDist > maxDistance) {
+					maxDistance = tempDist;
+				}
+			}
+		}
+		double adjustedGamma = GAMMA/maxDistance;
+		
 		double alpha = ALPHA;	// Allow alpha to be reduced
 		int currentGeneration = 0;
 		while (currentGeneration < MAX_GENERATION) {
@@ -47,7 +64,7 @@ public class Main implements FAConstants {
 						// Get distance r from firefly i to j
 						double r = calculateDistance(fireflies[i].getPosition(), fireflies[j].getPosition());
 						// Calculate attractiveness
-						double beta = BETA0*Math.exp(-GAMMA*r*r);
+						double beta = BETA0*Math.exp(-adjustedGamma*r*r);
 						// Move firefly i towards j
 						moveFirefly(alpha, beta, i, j);
 						// Update firefly i's intensity
@@ -60,21 +77,29 @@ public class Main implements FAConstants {
 			alpha = alpha*DELTA;
 			currentGeneration++;
 		}
-		long algTime = System.nanoTime();
-		System.out.println("\tAlgorithm completed in " + (algTime - initTime) / 1000000 + " ms");
+		//long algTime = System.nanoTime();
+		//System.out.println("\tAlgorithm completed in " + (algTime - initTime) / 1000000 + " ms");
 		
-		System.out.println("Calculating Results:");
+		//System.out.println("Calculating Results:");
 		double bestIntensity = fireflies[0].getIntensity();
+		int best = 0;
 		for (int i = 1; i < NUM_FIREFLIES; i++) {
 			if (fireflies[i].getIntensity() > bestIntensity) {
 				bestIntensity = fireflies[i].getIntensity();
+				best = i;
 			}
 		}
-		System.out.println("\tBest Value: " + 1/bestIntensity);
+
 		long endTime = System.nanoTime();
-		System.out.println("\tResult calculation time: " + (endTime - algTime) / 1000 + " us");
-		System.out.println("Total time taken: " + (endTime - startTime) / 1000000 + " ms");
-		System.out.println("Number of function evaluations: " + numEvaluations);
+		//System.out.println("\tResult calculation time: " + (endTime - algTime) / 1000 + " us");
+		
+		System.out.println("Total time taken: " + (endTime - startTime) / 1000000 + " ms\n");
+		System.out.println("Number of function evaluations: " + numEvaluations + "\n");
+		System.out.println("Best Value: " + 1/bestIntensity + "\n");
+		System.out.println("Located at:");		
+		for (int i = 0; i < NUM_DIMENSIONS; i++) {
+			System.out.println("\t" + fireflies[best].getPosition()[i]);
+		}
 	}
 	
 	/**
@@ -83,8 +108,8 @@ public class Main implements FAConstants {
 	 */
 	public static double calculateDistance(double[] position1, double[] position2) {
 		double sum = 0;
-		for (int i = 0; i < NUM_DIMENSIONS; i++) {
-			sum += Math.pow(position2[i]-position1[i],2);
+		for (int d = 0; d < NUM_DIMENSIONS; d++) {
+			sum += Math.pow(position2[d]-position1[d],2);
 		}
 		return Math.sqrt(sum);
 	}
@@ -95,11 +120,19 @@ public class Main implements FAConstants {
 	public static void moveFirefly(double alpha, double beta, int i, int j) {
 		double position;
 		Random r = new Random();
-		for (int k = 0; k < NUM_DIMENSIONS; k++) {
-			position = fireflies[i].getPosition()[k];
-			position += beta*(fireflies[j].getPosition()[k]-position) + ALPHA*r.nextGaussian();
-			fireflies[i].setPosition(position, k);
+		for (int d = 0; d < NUM_DIMENSIONS; d++) {
+			position = fireflies[i].getPosition()[d];
+			position += beta*(fireflies[j].getPosition()[d]-position) + ALPHA*(r.nextDouble()-0.5)*BOUND;
 			//ALPHA*r.nextGaussian()
+			
+			// Make sure the firefly stays within the bounds
+			if (position > BOUND) {
+				position = BOUND;
+			}
+			if (position < -BOUND) {
+				position = -BOUND;
+			}
+			fireflies[i].setPosition(position, d);
 		}
 	}
 }
--- src/fa/ProblemSet.java
+++ src/fa/ProblemSet.java
@@ -7,6 +7,10 @@ package fa;
  */
 public class ProblemSet {
 	
+	enum FunctionTypes {
+		SPHERE, ROSENBROCK, GRIEWANK, MICHALEWICZ, RASTRIGIN, HUMP, ESL
+	}
+	
 	private static final FunctionTypes FUNCTIONTYPE = FunctionTypes.ROSENBROCK;
 	private int numDimensions;
 	
@@ -29,7 +33,7 @@ public class ProblemSet {
 				break;
 			case ROSENBROCK:
 				for (int i = 0; i < numDimensions-1; i++) {
-					fitnessValue += 100*Math.pow(Math.pow(point[i],2)-point[i+1],2)+Math.pow(point[i]-1,2);
+					fitnessValue += 100*Math.pow( Math.pow(point[i],2)-point[i+1],2) + Math.pow(point[i]-1,2);
 				}
 				break;
 			case GRIEWANK:
@@ -42,6 +46,26 @@ public class ProblemSet {
 					p = p*Math.cos(point[i-1]/Math.sqrt(i));
 				}
 				fitnessValue = s/4000-p+1;
+				break;
+			case MICHALEWICZ:	// For m=10
+				for (int i = 0; i < numDimensions; i++) {
+					fitnessValue += Math.sin(point[i])*Math.pow( Math.sin((i+1)*Math.pow(point[i],2)/Math.PI) ,20);
+				}
+				fitnessValue = -fitnessValue;
+				break;
+			case RASTRIGIN:
+				for (int i = 0; i < numDimensions; i++) {
+					fitnessValue += Math.pow(point[i],2) - 10*Math.cos(2*Math.PI*point[i]);
+				}
+				fitnessValue += 10*numDimensions;
+				break;
+			case HUMP:	// Global minima: (0.0898, -0.7126), (-0.0898, 0.7126)
+				if (numDimensions == 2) {
+					fitnessValue = 1.0316285+4*Math.pow(point[0],2)-2.1*Math.pow(point[0],4)+Math.pow(point[0],6)/3+point[0]*point[1]-4*Math.pow(point[1],2)+4*Math.pow(point[1],4);
+				}
+				break;
+			case ESL:	// Emission Source Localization
+				
 				break;
 			default:
 				break;
